7d2e8bcd48bfd9a16be49387a96bb597806f051d
Koji Ishii <kojishi@gmail.com>
2013-09-21 03:59:32 +0900

    convertFromUNC

--- WinBridgeTests/WinBridgeTests.m
+++ WinBridgeTests/WinBridgeTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "WinBridgeTests.h"
+#import "WBUNC.h"
 
 @implementation WinBridgeTests
 
@@ -24,9 +25,31 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testStringFromUNC
 {
-    STFail(@"Unit tests are not implemented yet in WinBridgeTests");
+    // Normal cases
+    STAssertTrue([[WBUNC stringFromUNC:@"\\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Normal");
+
+    // Not a UNC
+    STAssertNil([WBUNC stringFromUNC:@"\\server\\share\\dir\\file"], @"Not a UNC");
+
+    // Preceding spaces
+    STAssertTrue([[WBUNC stringFromUNC:@"   \\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Pre-spaces");
+    STAssertTrue([[WBUNC stringFromUNC:@"¥t¥t\\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Pre-tabs");
+    STAssertTrue([[WBUNC stringFromUNC:@"¥t¥t   \\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Pre-spaces-tabs");
+
+    // Post spaces
+    STAssertTrue([[WBUNC stringFromUNC:@"\\\\server\\share\\dir\\file  "] isEqualToString:@"smb://server/share/dir/file"], @"Post-spaces");
+    STAssertTrue([[WBUNC stringFromUNC:@"\\\\server\\share\\dir\\fil e"] isEqualToString:@"smb://server/share/dir/fil e"], @"Post-spaces");
+    STAssertTrue([[WBUNC stringFromUNC:@"\\\\server\\share\\dir\\fil e  "] isEqualToString:@"smb://server/share/dir/fil e"], @"Post-spaces");
+    STAssertTrue([[WBUNC stringFromUNC:@" \\\\server\\share\\dir\\fil e  "] isEqualToString:@"smb://server/share/dir/fil e"], @"Post-spaces");
+
+    // Preceding extra text
+    STAssertTrue([[WBUNC stringFromUNC:@"Hello: \\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Pre-text");
+    STAssertTrue([[WBUNC stringFromUNC:@"He\\: \\\\server\\share\\dir\\file"] isEqualToString:@"smb://server/share/dir/file"], @"Pre-backslash");
+
+    // i18n
+    STAssertTrue([[WBUNC stringFromUNC:@"\\\\server\\s¥u3042\\d¥u3042\\f¥u3042"] isEqualToString:@"smb://server/s¥u3042/d¥u3042/f¥u3042"], @"Pre-text");
 }
 
 @end
